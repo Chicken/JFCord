5836fb33c48b2cec901addf8c385152e14bf5273
Antti Ellilä <antti@antti.codes>
2026-09-22 18:16:24 +0300

    fix indents

--- main.js
+++ main.js
@@ -519,7 +519,7 @@ let connectRPCTimeout;
         if (!data) return logger.warn("No selected server");
 
         jfc = new JFClient(data, {
-			appName: name,
+            appName: name,
             deviceName: name,
             deviceId: /** @type {string} */ (store.get("UUID")),
             deviceVersion: version,
@@ -701,7 +701,7 @@ let connectRPCTimeout;
         }
 
         let client = new JFClient(data, {
-			appName: name,
+            appName: name,
             deviceName: name,
             deviceId: /** @type {string} */ (store.get("UUID")),
             deviceVersion: version,
